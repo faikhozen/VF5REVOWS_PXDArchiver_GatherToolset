cb102ddc8ba125978847e1bd79b651b6813d926a
Fai Khozen <faikhozen@gmail.com>
2026-07-06 05:53:55 +0800

    retool

---
+++ ParTool/Options/Create.cs
@@ -0,0 +1,39 @@
+﻿// -------------------------------------------------------
+// © Kaplas. Licensed under MIT. See LICENSE for details.
+// -------------------------------------------------------
+namespace ParTool.Options
+{
+    using CommandLine;
+
+    /// <summary>
+    /// PAR archive extract options.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Class is passed as type parameter.")]
+    [Verb("create", HelpText = "Create a Yakuza PAR archive.")]
+    internal class Create
+    {
+        /// <summary>
+        /// Gets or sets the input directory.
+        /// </summary>
+        [Value(0, MetaName = "input", Required = true, HelpText = "Input directory.")]
+        public string InputDirectory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the PAR archive path.
+        /// </summary>
+        [Value(1, MetaName = "archive", Required = true, HelpText = "New Yakuza PAR archive path.")]
+        public string ParArchivePath { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the output file will be used in a Yakuza Kenzan, Yakuza 3, 4 or 5.
+        /// </summary>
+        [Option("alternative-mode", Default = false, HelpText = "Enable it if the PAR is for Yakuza 3, 4, 5 or Kenzan.")]
+        public bool AlternativeMode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the compression algorithm to use.
+        /// </summary>
+        [Option('c', "compression", Default = 0x01, HelpText = "SLLZ algorithm.")]
+        public int Compression { get; set; }
+    }
+}
---
+++ ParTool/Program.Extract.cs
@@ -0,0 +1,93 @@
+// -------------------------------------------------------
+// © Kaplas. Licensed under MIT. See LICENSE for details.
+// -------------------------------------------------------
+namespace ParTool
+{
+    using System;
+    using System.IO;
+    using ParLibrary;
+    using ParLibrary.Converter;
+    using Yarhl.FileSystem;
+    using Yarhl.IO;
+
+    /// <summary>
+    /// Extract contents functionality.
+    /// </summary>
+    internal static partial class Program
+    {
+        private static void Extract(Options.Extract opts)
+        {
+            WriteHeader();
+
+            if (!File.Exists(opts.ParArchivePath))
+            {
+                Console.WriteLine($"ERROR: \"{opts.ParArchivePath}\" not found!!!!");
+                return;
+            }
+
+            if (Directory.Exists(opts.OutputDirectory))
+            {
+                Console.WriteLine("WARNING: Output directory already exists. Its contents may be overwritten.");
+                Console.Write("Continue? (y/N) ");
+                string answer = Console.ReadLine();
+                if (!string.IsNullOrEmpty(answer) && answer.ToUpperInvariant() != "Y")
+                {
+                    Console.WriteLine("CANCELLED BY USER.");
+                    return;
+                }
+            }
+
+            Directory.CreateDirectory(opts.OutputDirectory);
+
+            var parameters = new ParArchiveReaderParameters
+            {
+                Recursive = opts.Recursive,
+            };
+
+            using Node par = NodeFactory.FromFile(opts.ParArchivePath, Yarhl.IO.FileOpenMode.Read);
+            par.TransformWith(new ParArchiveReader(parameters));
+
+            Extract(par, opts.OutputDirectory);
+        }
+
+        private static void Extract(Node parNode, string outputFolder)
+        {
+            foreach (Node node in Navigator.IterateNodes(parNode))
+            {
+                string fileInfoPath = node.Path.Substring(parNode.Path.Length).Replace('/', Path.DirectorySeparatorChar);
+                string outputPath = Path.Join(outputFolder, fileInfoPath);
+
+                var file = node.GetFormatAs<ParFile>();
+
+                if (file == null)
+                {
+                    Directory.CreateDirectory(outputPath);
+                    continue;
+                }
+
+                Console.Write($"Extracting {node.Path}... ");
+
+                if (file.IsCompressed)
+                {
+                    node.TransformWith<ParLibrary.Sllz.Decompressor>();
+                }
+
+                if (node.Stream.Length > 0)
+                {
+                    node.Stream.WriteTo(outputPath);
+                }
+                else
+                {
+                    // Create empty file
+                    File.Create(outputPath).Dispose();
+                }
+
+                File.SetCreationTime(outputPath, file.FileDate);
+                File.SetLastWriteTime(outputPath, file.FileDate);
+                File.SetAttributes(outputPath, (FileAttributes)file.Attributes);
+
+                Console.WriteLine("DONE!");
+            }
+        }
+    }
+}
